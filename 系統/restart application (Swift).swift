ef836a7db5111dd4ca4851ff51f353a7f03453b7
Woody Liu <woody_liu@cmoney.com.tw>
2020-12-09 10:17:15 +0800

    feat: 添加重啟動APP 添加transition animate 效果

--- 系統/restart application (Swift).swift
+++ 系統/restart application (Swift).swift
@@ -1,5 +1,5 @@
 
-/// 重新啟動app
+    /// 重新啟動app
     func restartApplication() {
         let controller = yourViewController()
         let nav = UINavigationController(rootViewController: controller)
@@ -17,5 +17,30 @@
             }
             UIApplication.shared.windows.filter{$0.isKeyWindow}.first?.rootViewController = nav
             
+        }
+    }
+
+
+    /// 重新啟動有動畫 (適合重登)
+    func restartApplication() {
+        let controller = yourViewController()
+        let nav = UINavigationController(rootViewController: controller)
+        
+        if #available(iOS 13, *) {
+            //移除所有ViewController
+            UIApplication.shared.windows.forEach {
+                $0.subviews.forEach { $0.removeFromSuperview() }
+            }
+            //重開ViewController
+            UIApplication.shared.windows.filter{$0.isKeyWindow}.first?.rootViewController = nav
+        }else {
+            UIApplication.shared.keyWindow?.subviews.forEach{
+                $0.removeFromSuperview()
+            }
+            guard let window = (UIApplication.shared.windows.filter{$0.isKeyWindow}).first else { return }
+             window.rootViewController = nav
+            // option: 選擇動畫類型
+             UIView.transition(with: window, duration: 0.3, options: .transitionFlipFromBottom, animations: nil, completion: nil)
+
         }
     }
