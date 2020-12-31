d9015e251683a77e07b0e5b9638dcf783e9a9126
Woody Liu <woody_liu@cmoney.com.tw>
2020-12-31 14:36:14 +0800

    feat: alertAction title換色方法

---
+++ UIKit/UIAlertAction title 換色.swift
@@ -0,0 +1,10 @@
+
+extension UIAlertAction {
+    var titleTextColor: UIColor? {
+        get {
+            return self.value(forKey: "titleTextColor") as? UIColor
+        } set {
+            self.setValue(newValue, forKey: "titleTextColor")
+        }
+    }
+}
