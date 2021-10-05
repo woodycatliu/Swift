75607483bce182506bba80d8ffc748bc9af66773
Woody Liu <bestlifeforzhi@gmail.com>
2021-10-05 12:17:37 +0800

    fix:  SwipeNavigationController

--- UIKit/SwipeNavigationController.swift
+++ UIKit/SwipeNavigationController.swift
@@ -1,23 +1,27 @@
-
-import UIKit
+import UIKit 
 
 class SwipeNavigationController: UINavigationController {
+        
+    // 是否正在做 push Anumation
+    // 用來阻擋 popanimaiotn 手勢被觸發
     fileprivate var duringPushAnimation = false
     
     override init(rootViewController: UIViewController) {
         super.init(rootViewController: rootViewController)
-        delegate = self
+
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        delegate = self
+        
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         interactivePopGestureRecognizer?.delegate = self
-        self.view.addGestureRecognizer(self.fullScreenSwipeGestureRecongnizer)
+        fullScreenSwipeGestureRecongnizer.delegate = self
+        delegate = self
+        view.addGestureRecognizer(fullScreenSwipeGestureRecongnizer)
     }
     
     override func pushViewController(_ viewController: UIViewController, animated: Bool) {
@@ -30,10 +34,11 @@ class SwipeNavigationController: UINavigationController {
     private lazy var fullScreenSwipeGestureRecongnizer: UIPanGestureRecognizer = {
         let gestureRecognizer = UIPanGestureRecognizer()
         
-        if let cachedInteractionController = self.value(forKey: SwipePopTransitionKey.controller.rawValue) as? NSObject {
-            let action = SwipePopTransitionKey.selector.rawValue
+        if let cachedInteractionController = self.value(forKey: SwipePopTransitionKey.controllerKey) as? NSObject {
+            let action = SwipePopTransitionKey.selectorKey
             let selector = Selector(action)
             if cachedInteractionController.responds(to: selector) {
+                gestureRecognizer.addTarget(self, action: #selector(controlGlobalPlayerShowingTiming))
                 gestureRecognizer.addTarget(cachedInteractionController, action: selector)
             }
         }
@@ -56,14 +61,21 @@ extension SwipeNavigationController: UINavigationControllerDelegate {
 extension SwipeNavigationController: UIGestureRecognizerDelegate {
     
     func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
-        guard gestureRecognizer == interactivePopGestureRecognizer else {
+        
+        // 擋住原生前端（1cm)的滑動手勢
+        if gestureRecognizer == interactivePopGestureRecognizer {
+            return false
+        }
+        
+        // 非自定義的手勢，回傳true
+        guard gestureRecognizer == fullScreenSwipeGestureRecongnizer else {
             return true // default value
         }
         
         // Disable pop gesture in two situations:
         // 1) when the pop animation is in progress
         // 2) when user swipes quickly a couple of times and animations don't have time to be performed
-        return viewControllers.count > 1 && duringPushAnimation == false
+        return viewControllers.count > 1 && !duringPushAnimation
     }
     
     
@@ -78,9 +90,12 @@ extension SwipeNavigationController: UIGestureRecognizerDelegate {
 
 extension SwipeNavigationController {
     
-    enum SwipePopTransitionKey: String {
-        case controller = "_cachedInteractionController"
-        case selector = "handleNavigationTransition:"
+    enum SwipePopTransitionKey {
+        
+        static var controllerKey: String { return "_cachedInter" + "actionController" }
+        
+        static var selectorKey: String { return "handleNavig" + "ationTransition:" }
+        
     }
     
 }
