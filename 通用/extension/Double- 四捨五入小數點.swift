cc42fd19e3b1439afcb07482bcb398e7d0d6135e
Woody Liu <bestlifeforzhi@gmail.com>
2021-12-28 16:54:28 +0800

    refactor:  double rounding extension

--- 通用/extension/Double- 四捨五入小數點.swift
+++ 通用/extension/Double- 四捨五入小數點.swift
@@ -3,12 +3,10 @@ import Foundation
 
 extension Double {
     
-    func rounding(position: Int16)-> Double {
-        let hundler = NSDecimalNumberHandler(roundingMode: .plain, scale: position, raiseOnExactness: false, raiseOnOverflow: false, raiseOnUnderflow: false, raiseOnDivideByZero: true)
-        let nsNumber = NSDecimalNumber(value: self)
-        let newNumber = nsNumber.rounding(accordingToBehavior: hundler)
-        
-        return Double(newNumber.stringValue) ?? Double(Int(self * 10 * Double(position)) / 10 / Int(position))
+    func rounding(to position: Int)-> Double {
+        var origin = Decimal(self)
+        var willRound = Decimal()
+        NSDecimalRound(&willRound, &origin, position, .plain)
+        return NSDecimalNumber(decimal: willRound).doubleValue
     }
-    
 }
