c839bc6922fa9ae0d73d14243b61c08568a293c7
Woody Liu <bestlifeforzhi@gmail.com>
2022-01-11 12:51:35 +0800

    docs: add Combine floder feat: add UITextField extension to textPublisher

---
+++ Combine/Extension/TextFieldPulisher_Extension.swift
@@ -0,0 +1,18 @@
+//
+//  TextFieldExtension.swift
+//  ReactiveProgrammingFramework_MemoryLeak
+//
+//  Created by Woody Liu on 2022/1/3.
+//
+
+import UIKit
+import Combine
+
+extension UITextField {
+    func textPublisher()-> AnyPublisher<String, Never> {
+        NotificationCenter.default
+            .publisher(for: UITextField.textDidChangeNotification, object: self)
+            .map { ($0.object as? UITextField)?.text ?? "" }
+            .eraseToAnyPublisher()
+    }
+}
