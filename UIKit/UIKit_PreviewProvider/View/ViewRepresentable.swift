28f835709821b396cbb12411e25c64cf215f7845
Woody <woodtliutvbs@innov.tvbs.com.tw>
2022-02-11 10:10:36 +0800

    feat: removing unuse code

--- UIKit/UIKit_PreviewProvider/View/ViewRepresentable.swift
+++ UIKit/UIKit_PreviewProvider/View/ViewRepresentable.swift
@@ -10,13 +10,6 @@ import Foundation
 #if canImport(SwiftUI) && DEBUG
 import SwiftUI
 
-extension ViewProvider {
-    // Sample
-    
-    
-    
-}
-
 @frozen public struct ViewRepresentable: UIViewRepresentable {
     let view: UIView
     public func makeUIView(context: Context) -> some UIView {
