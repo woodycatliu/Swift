a4c03b2c41f4c3e53b3614219ff906c12cc89566
Woody <woodtliutvbs@innov.tvbs.com.tw>
2022-03-03 10:55:56 +0800

    update

---
+++ Combine/DidSetPublished.swift
@@ -0,0 +1,32 @@
+import Combine
+import Foundation
+
+@propertyWrapper
+class DidSetPublished<Value> {
+    private var _value: Value
+    private let subbject: PassthroughSubject<Value, Never> = .init()
+    var wrappedValue: Value {
+        set {
+            _value = newValue
+            subbject.send(_value)
+        }
+        
+        get { return _value }
+        
+    }
+    var projectedValue: PassthroughSubject<Value, Never> {
+        return subbject
+    }
+    
+    init(wrappedValue value: Value) {
+        _value = value
+        wrappedValue = value
+    }
+}
+
+// Another Soulation
+extension Published.Publisher where Failure == Never {
+    var didSet: AnyPublisher<Output, Never> {
+        return self.receive(on: RunLoop.main).eraseToAnyPublisher()
+    }
+}
